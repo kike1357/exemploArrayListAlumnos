7372151c99ea754cd574fe2800b39b43bfad2390
epastorizalorenzo <epastorizalorenzo@atanasoff25.danielcastelao.org>
2016-02-17 11:25:11 +0100

    commit gitHub

---
+++ src/exemploarraylistalumnos/ExemploArrayListAlumnos.java
@@ -0,0 +1,18 @@
+package exemploarraylistalumnos;
+
+/**
+ *
+ * @author epastorizalorenzo
+ */
+public class ExemploArrayListAlumnos {
+
+    
+    public static void main(String[] args) {
+        GrupoAlumno obxAlum= new GrupoAlumno();
+        obxAlum.crearLista();
+        obxAlum.verLista();
+        obxAlum.verIgual();   
+    }
+    }
+    
+
